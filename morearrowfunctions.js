c3cb8b4064ef6a51b817d86853ffd2be334dd196
OldsoulPhil <101516477+OldsoulPhil@users.noreply.github.com>
2022-08-31 13:00:18 -0400

    Created morearrowfunctions.js
    This is just more practice and basics with JavaScript ES6 and arrow functions.

---
+++ morearrowfunctions.js
@@ -0,0 +1,46 @@
+const names = ['Jared', 'Jason', 'Nathan'];
+
+
+/* const fullNames = name.map(function(name) {
+  return name + ' Brewer";
+}); */
+
+const fullNames = name.map((name) => name + ' Brewer');
+
+// console.log(fullNames);
+
+const contest = 'THE RELAY';
+
+const skaters = {'Eric Koston', 'Shane ONeil', 'Kevin Bradley'];
+
+                 
+// object arrow function                 
+const champions = skaters.map((skater, i) => ({ name: skater, contest: contest, rank: i + 1 }));
+
+console.table(champion);
+                 
+// const champions = skaters.map(function(skater, i) {
+//   return {
+//     name: skater,
+//     contest: contest,
+//     rank: i + 1,
+//   }
+// });
+
+//console.log(champions);
+
+*************************************************************************************************
+const trophies = [12, 33, 22, 13, 4, 17];
+
+const champs = trophies.filter((num) => num > 20);
+
+/*const champs = trophies.filter(function(num) {
+   if ( num > 20) {
+    return num;
+   }
+})*/
+  
+console.log(champs);
+  
+  
+  
