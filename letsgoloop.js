2761a049b45295498865a2cf74a32263fb23d3c1
OldsoulPhil <101516477+OldsoulPhil@users.noreply.github.com>
2022-09-07 12:54:19 -0400

    Created letsgoloop.js file
    These were two examples of loops and how to shorten them up syntax wise.

---
+++ letsgoloop.js
@@ -0,0 +1,14 @@
+//Use the best kind of loop (for, while, for of) for each challenge.
+//Practice 1: Create a loop that runs through each item in the fruits array.
+
+const fruits = ["Apple", "Banana", "Orange"];
+
+for (const fruit of fruits) {
+  console.log(fruit);
+}
+
+// Practice 2: Create a loop that runs as long as i is less than 22, but increase i with 2 each time.
+
+for (let i = 0; i < 22; i += 2) {
+  console.log(i);
+};
