87d14c004ccd0175ed8045336e80e391826999c0
OldsoulPhil <101516477+OldsoulPhil@users.noreply.github.com>
2022-09-08 19:01:06 -0400

    Updated set.js
    just update the set file to finish out the lesson for set

--- set.js
+++ set.js
@@ -6,16 +6,34 @@
 // cant access items individualy
 // not index based
 
-const names = new Set();
+const people = ['Jason', 'Nathan', 'Jared'];
+
+const names = new Set(people);
 
 // add items
-names.add('Jason');
-names.add('Nathan');
-names.add('Jared');
+// names.add('Jason');
+// names.add('Nathan');
+// names.add('Jared');
+
+// names.add('Bobby');
+
+// when using a set key and values are the same thing
+const humans = names.values();
+
+// for(const name of humans) {
+//   console.log(name);
+// }
 
 // delete item
 //names.delete('Jason');
-console.log(names);
-names.clear();
-console.log(names);
+
 //console.log(names.size);
+
+// logs out set clears it then sets a clear one
+// console.log(names);
+// names.clear();
+// console.log(names);
+
+console.log(names.has('Jason')); // jason vs Jason has to be an exact match
+//console.log(names);
+// in the teaching they use .values(), .keys(), .entries
