50b01533fd86aadce19299c31ef7da7f78475ba0
OldsoulPhil <101516477+OldsoulPhil@users.noreply.github.com>
2022-09-11 20:21:22 -0400

    Created dryprinciple.js file
    this lesson taught me what the DRY principle is.

---
+++ dryprinciple.js
@@ -0,0 +1,12 @@
+// JavaScript the DRY principle (dont repeat yourself)
+// "A principle of software development aimed at reducing repetition of information of all kinds
+
+// Reduce repitition
+// Lowers lines of code
+// more maintainable code
+// easier to read
+// clean code
+// this principle applys in all languages
+
+
+
