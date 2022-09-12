9222cf8890b9c6975c49dc638be129af26e12c24
OldsoulPhil <101516477+OldsoulPhil@users.noreply.github.com>
2022-09-11 20:44:42 -0400

    Created srp.js file
    this lesson just taught me a few things on Single Responsibility Principle.

---
+++ srp.js
@@ -0,0 +1,10 @@
+// JavaScript SRP Single Responsibility Principle
+
+// Each class and module in a program should focus on a single task
+// Example: Printer
+
+// Open/Close Principle
+// Liskov Substitutional Principle
+// Interface Segregation Principle
+// Dependancy Inversion Principle
+
