7fada19479bbfccfbc210f5f15be722212d03e73
OldsoulPhil <101516477+OldsoulPhil@users.noreply.github.com>
2022-09-23 13:35:47 -0400

    Updated spreadoperator.js file
    moved the order of how you should learn about the  spread operator.

--- spreadoperator.js
+++ spreadoperator.js
@@ -3,6 +3,20 @@
 // [...array]; ... is the spread operator
 // allows us to spread out the items in an itterable arrays, strings, objects, dom nodes, map sets
 
+// 1.
+// plugged out each item in the skatePants
+//console.log(...skatePants);
+
+// 2.
+// let combinedIsh = skatePants.concat(skateShoes);
+// console.log(combinedIsh);
+
+// 3.
+// let combinedIsh = [];
+// combinedIsh = combinedIsh.concat(skatePants);
+// combinedIsh.push('Santa Cruz');
+// combinedIsh = combinedIsh.concat(skateShoes);
+// console.log(combinedIsh);
 
 // 4.
 const skatePants = ['Volcom', 'Matix', 'RVCA'];
@@ -40,19 +54,3 @@ console.log(niceComments)
 //console.log(...skaterDude.shoes[2]);
 // console.log([...skaterDude.shoes[2]]);
 
-
-// 3.
-// let combinedIsh = [];
-// combinedIsh = combinedIsh.concat(skatePants);
-// combinedIsh.push('Santa Cruz');
-// combinedIsh = combinedIsh.concat(skateShoes);
-// console.log(combinedIsh);
-
-
-// 2.
-// let combinedIsh = skatePants.concat(skateShoes);
-// console.log(combinedIsh);
-
-// 1.
-// plugged out each item in the skatePants
-//console.log(...skatePants);
